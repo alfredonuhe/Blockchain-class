9f5c4d41cbb56bc39ec7b3bdfa015eaa19198ec3
alfredonuhe <alfredonuhe@gmail.com>
2017-10-01 17:55:44 +0200

    Modifications in index and js files
    Modified index with Bootstrap conventions and added new functionalities to the javascript file

--- hash_256.js
+++ hash_256.js
@@ -1,3 +1,6 @@
+/*Use the 3-5-1 combination in order to check mechanism of
+the simulation */
+
 var imput_ids = ["#Block_number_new","#merkle_root_new","#dificulty_new","#nonce_new"];
 var hash_message= '';
 
@@ -8,12 +11,36 @@ function isNumber(n) {
 
 //Checks for mining result
 function correctHash(hash, dificulty) {
+    correctHashStyle(false);
+    $("#block_hash_new").val(hash);
     for (var i = 0; i < dificulty; i++) {
       if (hash.charAt(i) != "0") {
-          return false;
+          return;
       }
     }
-    return true;
+    correctHashStyle(true);
+    return;
+}
+
+//Sets style for correct hash
+function correctHashStyle(correct){
+    if (correct) {
+      $("#block_hash_new")
+      .css("border","3px solid #34ab30")
+      .css("background-color","#eaffef");
+      return;
+    }
+    $("#block_hash_new")
+    .css("border","1px solid #ccc")
+    .css("background-color","#eee");
+}
+
+//sets new nonce for new search
+function newNonce(){
+    var lastNonce = parseInt($("#nonce_new").val()) +1;
+    console.log(lastNonce);
+    $("#nonce_new").val(lastNonce);
+    console.log($("#nonce_new").val());
 }
 
 //Concatenates all of the values of the form
@@ -33,13 +60,11 @@ function concatById(imput_ids) {
 
 $(document).ready(function() {
     $("#hash_button_new").click(function(){
-        if(concatById(imput_ids , hash_message)) return;
+        if(concatById(imput_ids)) return;
         console.log(hash_message);
         var hash = Crypto.SHA256(String(hash_message));
         console.log(hash);
-        $("#block_hash_new").attr("value", hash)
-        if (correctHash(hash,$(imput_ids[2]).val())) {
-          $("#block_hash_new").css("border","3px solid #34ab30")
-        }
+        newNonce();
+        correctHash(hash,$(imput_ids[2]).val());
     });
 });
