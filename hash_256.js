44334b5fa5f6eeca7d893e372ddd09578877f25d
alfredonuhe <100315960@alumnos.uc3m.es>
2017-10-12 14:47:17 +0200

    Finished logical functionality

--- hash_256.js
+++ hash_256.js
@@ -1,11 +1,10 @@
-/*Use the 3-5-1 combination in order to check mechanism of
-the simulation */
-
 //Defining global variables
-var imput_ids = ["#Block_number_new","#merkle_root_new","#dificulty_new","#nonce_new"];
+var input_data = [new infoInput("#version_new", 4,null,"Version",null), new infoInput("#block_number_new", 4,null,"Block #",null),
+    new infoInput("#prev_block_new", 64,null,"Previous block",null),new infoInput("#dificulty_new", 1,null,"Dificulty",null),
+    new infoInput("#timestamp_new", 10,null,"Timestamp",null), new infoInput("#merkle_root_new", 64,null,"Merkle root",null),
+    new infoInput("#nonce_new", 10,null,"Nonce",null)];
 var hash_message= '';
 var button_disabled = false;
-//var timer = true;
 var info_array = [];
 var new_queue= new Queue();
 
@@ -20,13 +19,28 @@ function infoBlock(hash, hash_message) {
     this.hash_message= hash_message;
 }
 
+// Hash info input
+function infoInput(id, allowed_length, real_length, name, value) {
+    this.id= id;
+    this.allowed_length= allowed_length;
+    this.real_length= real_length;
+    this.name= name;
+    this.value= value;
+}
+
 // Disables 'Try' button after finding a correct hash
 function disableButton(){
   button_disabled = true;
   console.log("disabled");
+  $("#hash_button_new")
+      .css("background-color","#c5c5c5")
+      .css("border-color","#eaeaea");
   window.setTimeout(function(){
-    console.log("enabled");
-    button_disabled = false;}
+      console.log("enabled");
+      $("#hash_button_new")
+        .css("background-color","#337ab7")
+        .css("border-color","#2e6da4");
+      button_disabled = false;}
   ,5000);
 }
 
@@ -34,17 +48,16 @@ function disableButton(){
 function correctHash(hash, dificulty) {
     correctHashStyle(false);
     $("#block_hash_new").val(hash);
+    newNonce();
     for (var i = 0; i < dificulty; i++) {
       if (hash.charAt(i) != "0") {
           return;
       }
     }
+    $("#prev_block_new").val(hash);
     correctHashStyle(true);
     disableButton();
-    resfreshPannel(hash, hash_message);
-    console.log(hash);
-    console.log(hash_message);
-    post_Hash(hash, hash_message);
+    refreshPannel(hash, hash_message);
     return;
 }
 
@@ -63,22 +76,24 @@ function correctHashStyle(correct){
 
 //sets new nonce for new search
 function newNonce(){
-    var lastNonce = parseInt($("#nonce_new").val()) +1;
-    $("#nonce_new").val(lastNonce);
+    var nonce = parseInt($("#nonce_new").val()) +1;
+    $("#nonce_new").val(nonce);
 }
 
-//Concatenates all of the values of the form
-//only if all data is providad in the form
-function concatById(imput_ids) {
+/*Concatenates all of the values of the form
+only if all data is providad in the form*/
+function concatById(input_data) {
+  $("#timestamp_new").val(calcTimestamp());
   hash_message='';
-  for (i = 0; i < imput_ids.length; i++) {
-      if(!isNumber($(imput_ids[i]).val())){
-        alert("Enter correct values " + i);
+  for (i = 0; i < input_data.length; i++) {
+      input_data[i].real_length = $(input_data[i].id).val().length;
+      if(input_data[i].real_length > input_data[i].allowed_length){
+        alert("Maximum number of digits in field " + input_data[i].name + " is of " + input_data[i].allowed_length + " digits.");
         return false;
       }
-      hash_message = hash_message.concat($(imput_ids[i]).val());
+      input_data[i].value = frontalPadding($(input_data[i].id).val(), input_data[i].allowed_length);
+      hash_message = hash_message.concat(input_data[i].value);
   }
-  //console.log(hash_message);
   return true;
 }
 
@@ -91,133 +106,53 @@ function post_Hash(hash, hash_message){
         });
 }
 
-//shift left array
-function shiftLeft(array){
-  for (var i = 0; i < array.length-1; i++) {
-    array[i+1]=array[i]
-  }
-  array[0]= undefined;
-}
-
-//check if array is full
-function isFull(array){
-  for (var i = 0; i < array.length; i++) {
-    if (array[i]==undefined) {
-      return false;
-    }
-  }
-  return true;
-}
-
-//fill in empty spot
-function fillFirst(array, value){
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] == 'undefined') {
-      array[i]= value;
-      return true;
-    }
-  }
-  return false;
+//Creates random hash
+function randomHash(){
+  return Crypto.SHA256(String(Math.random()));
 }
 
 //test hiding and showing hash text areas
-function resfreshPannel(hash, hash_message){
+function refreshPannel(hash, hash_message){
+  //eliminar
   var infoBlock_aux = new infoBlock(hash, hash_message);
-  var dinamic_board = document.getElementsByClassName("dinamic_board");
-  if (new_queue.size() == 5) {
-    new_queue.dequeue();
-    new_queue.enqueue(infoBlock_aux);
-  }else {
-    new_queue.enqueue(infoBlock_aux);
+  $("#dinamic_board_alfredo").val("BLOCK HASH: " + hash + "\n" + "BLOCK HEADER: " + hash_message + "\n\n");
+  var new_data;
+  for (var i = 0; i < input_data.length; i++) {
+    new_data = input_data[i].name.toUpperCase() + ": " + input_data[i].value + "\n";
+    $('#dinamic_board_alfredo').val($('#dinamic_board_alfredo').val() + new_data);
   }
-  for (var i = 0; i < dinamic_board.length; i++) {
-    if (i<new_queue.size()) {
-      dinamic_board[i].style.display = "";
-      dinamic_board[i].value = new_queue._storage[i].hash
-    }else {
-      dinamic_board[i].style.display = "none";
-    }
+}
 
-  }
+//add frotal padding to data
+function frontalPadding(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
 
-/*
-  console.log("here");
-  console.log("here: " + dinamic_board[0].style.display);
-  var dinamic_board = document.getElementsByClassName("dinamic_board");
-  var time= 1000;
-    for (var i = 0; i < 1; i++) {
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[0].style.display = "none";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[1].style.display = "none";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[2].style.display = "none";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[3].style.display = "none";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[4].style.display = "none";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[5].style.display = "none";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      //oposite
-      setInterval(function(){
-          dinamic_board[0].style.display = "";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[1].style.display = "";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[2].style.display = "";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[3].style.display = "";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[4].style.display = "";
-      }, time);
-      time= time +1000;
-      console.log(time);
-      setInterval(function(){
-          dinamic_board[5].style.display = "";
-      }, time);
-      time= time +1000;
-      console.log(time);
-    }*/
+//Calculates form timestamp
+function calcTimestamp() {
+     return Math.floor((new Date).getTime()/1000);
+}
+
+//Initial form data
+function initialData() {
+    $("#timestamp_new").val(calcTimestamp());
+    $("#dificulty_new").val(1);
+    $("#merkle_root_new").val(randomHash());
+    $("#nonce_new").val(1);
 }
 
 $(document).ready(function() {
-
+    initialData();
+    console.log(input_data);
     $("#hash_button_new").click(function(){
         console.log(button_disabled);
-        if(!concatById(imput_ids) || button_disabled) return;
+        if(!concatById(input_data) || button_disabled) return;
         var hash = Crypto.SHA256(String(hash_message));
-        newNonce();
-        correctHash(hash,$(imput_ids[2]).val());
+        correctHash(hash,$("#dificulty_new").val());
+    });
+    $("#random_hash_new").click(function(){
+      $("#merkle_root_new").val(randomHash());
     });
 });
