81f00cdb3af27ea59f817cb9852e65f28ea61c20
alfredonuhe <100315960@alumnos.uc3m.es>
2018-06-15 00:18:30 +0200

    Deleted comments.

--- js/ms_exec.js
+++ js/ms_exec.js
@@ -1,30 +1,22 @@
-//Document execution of the Mining Simulator
+/**
+ * View handling for non-mobile and mobile devices.
+ */
 $(document).ready(function() {
 
     var mobileString = "";
     if (screen.width <= 480) mobileString = "-mobile";
 
-    console.log("#login-session" + mobileString + " new js");
-
     $("#login-session" + mobileString).click(function(){
-        //document.getElementById("login-register-tab" + mobileString).style.display = "none";
-        //document.getElementById("login-tab" + mobileString).style.display = "block";
         $("#login-register-tab" + mobileString).hide();
         $("#login-tab" + mobileString).show();
     });
 
     $("#register-session" + mobileString).click(function(){
-        //document.getElementById("login-register-tab" + mobileString).style.display = "none";
-        //document.getElementById("register-tab" + mobileString).style.display = "block";
         $("#login-register-tab" + mobileString).hide();
         $("#register-tab" + mobileString).show();
     });
 
     $(".go-back-btn" + mobileString).click(function(){
-        //document.getElementById("login-tab" + mobileString).style.display = "none";
-        //document.getElementById("register-tab" + mobileString).style.display = "none";
-        //document.getElementById("login-register-tab" + mobileString).style.display = "block";
-
         $("#register-tab" + mobileString).hide();
         $("#login-tab" + mobileString).hide();
         $("#login-register-tab" + mobileString).show();
